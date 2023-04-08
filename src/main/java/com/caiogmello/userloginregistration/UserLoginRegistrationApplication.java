f234ac9983db88f220927b7e70cdd0aef3f76028
caiogmello <caiogomedemello@gmail.com>
2023-04-08 15:49:01 -0300

    Initial commit

---
+++ src/main/java/com/caiogmello/userloginregistration/UserLoginRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.caiogmello.userloginregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserLoginRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserLoginRegistrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caiogmello/userloginregistration/UserLoginRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caiogmello.userloginregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserLoginRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
